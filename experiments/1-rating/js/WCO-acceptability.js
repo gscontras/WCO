e2b535bb965a77216c8436a137ea789edddd36ce
kghowitt <kghowitt@gmail.com>
2023-09-11 17:04:45 -0400

    updated experiment to interface with proliferate

--- experiments/1-rating/js/WCO-acceptability.js
+++ experiments/1-rating/js/WCO-acceptability.js
@@ -254,9 +254,7 @@ function make_slides(f) {
         subject_information: exp.subj_data,
         time_in_minutes: (Date.now() - exp.startT) / 60000,
       };
-      setTimeout(function () {
-        turk.submit(exp.data);
-      }, 1000);
+      proliferate.submit(exp.data);
     },
   });
 
