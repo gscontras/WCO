757c7d2c48b6c3f8680148cedc0bce63d4fc4e3a
kghowitt <kghowitt@gmail.com>
2024-03-10 15:45:58 -0400

    updated analysis script and added by participant histogram

--- experiments/1-SONA/analysis/WCO-analysis.R
+++ experiments/1-SONA/analysis/WCO-analysis.R
@@ -13,11 +13,8 @@ length(unique(df$workerid)) # 15
 unique(df$subject_information.language)
 d = df[df$subject_information.language=="English"|
        df$subject_information.language=="English "|
-       df$subject_information.language=="english" |
-       df$subject_information.language=="English, Filipino" |
-       df$subject_information.language=="English, Chinese" |
-       df$subject_information.language=="English, Arabic" |
-       df$subject_information.language=="English, Hindi" 
+       df$subject_information.language=="english"|
+       df$subject_information.language=="english "
        ,]
 length(unique(d$workerid)) # 10
 
@@ -37,23 +34,54 @@ d[d$item == "filler4" |
 d$filler_check = NA
 
 aggregate(response~item*filler_type*workerid,data=d,FUN=mean)
+
+d$filler_correct = 0
+d[d$condition=="filler" & d$filler_type=="good" & d$response>=0.50,]$filler_correct = 1
+d[d$condition=="filler" & d$filler_type=="bad" & d$response<0.50,]$filler_correct = 1
+
+e = d
+
+for(i in unique(as.factor(e$workerid))) {
+  tempsum <- sum(e[e$workerid==i,]$filler_correct)
+  if(tempsum<5) {
+    e = e[e$workerid != i,]
+  }
+}
+
+#summary(as.factor(e$workerid))
 # remove participants 76, 78, 84, 85, 89, 90, 103, 107, 99
-e = d[d$workerid!="30"&
-        d$workerid!="76"&
-        d$workerid!="78"&
-        d$workerid!="84"&
-        d$workerid!="85"&
-        d$workerid!="89"&
-        d$workerid!="90"&
-        d$workerid!="103"&
-        d$workerid!="107" &
-        d$workerid!="99"
-        ,]
+
+# e = d[d$workerid!="30"&
+#         d$workerid!="76"&
+#         d$workerid!="78"&
+#         d$workerid!="84"&
+#         d$workerid!="85"&
+#         d$workerid!="89"&
+#         d$workerid!="90"&
+#         d$workerid!="103"&
+#         d$workerid!="107" &
+#         d$workerid!="99"
+#         ,]
 
 
 # remove item with typo -> no longer needed, typo fixed
 #f = e[e$item!="priest",]
 
+f = matrix(unique(as.factor(e$workerid)), ncol = 1)
+f = as.data.frame(f)
+
+colnames(f)[1] <- "workerid"
+f$N_avg = 0
+f$Y_avg = 0
+
+for (i in unique(as.factor(e$workerid))){
+  f[f$workerid == i,]$N_avg = mean(e[e$workerid == i & (e$condition == "NDA" | e$condition == "NDI" | e$condition == "NQA" | e$condition == "NQI"),]$response)
+  f[f$workerid == i,]$Y_avg = mean(e[e$workerid == i & (e$condition == "YDA" | e$condition == "YDI" | e$condition == "YQA" | e$condition == "YQI"),]$response)
+  }
+
+f$avgDiff = f$N_avg - f$Y_avg
+
+hist(f$avgDiff)
 
 ## calculate averages and CIs by condition
 length(unique(e$workerid)) # 4
